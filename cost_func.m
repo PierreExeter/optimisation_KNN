b7d7c9e9462798f459dac01cb2eab75fe1beea63
Pierre Aumjaud <pierre.aumjaud@gmail.com>
2016-10-25 18:53:55 +0200

    add readme

--- cost_func.m
+++ cost_func.m
@@ -7,10 +7,6 @@ end
 y = zeros(1, 2);
 
 % write knn parameters to file
-
-% generate 5 random int between 1 and 50 (for the example)
-% x = randi([1 50],1, 5);
-
 fid = fopen('knn_params.txt','w');
 fprintf(fid,'%d\n', x);
 fclose(fid);
@@ -18,6 +14,7 @@ fclose(fid);
 % run model
 system('python knn_model.py');
 
+% read objectives
 fid = fopen('obj1.txt','r');
 y(1) = -fscanf(fid,'%f');
 fclose(fid);
@@ -25,5 +22,3 @@ fclose(fid);
 fid = fopen('obj2.txt','r');
 y(2) = -fscanf(fid, '%f');
 fclose(fid);
-
-end
